363c1efb2d21acbcfd478071e2a971cf5d8ed857
ronnniedev <veronicapersonal1995@gmail.com>
2024-07-17 16:40:28 +0200

    Funcion de audio para Youtube añadida

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands,tasks
+import os
+from dotenv import load_dotenv
+import yt_dlp as youtube_dl
+from discord import FFmpegPCMAudio
 # from modulos.bromas import *
 # importamos variables de apikeys
 import requests
@@ -7,6 +11,40 @@ import json
 from apikeys import *
 
 client = commands.Bot(command_prefix='!', intents=discord.Intents.all()) #Establece las propiedades del cliente, intents los permisos, lo inicializamos con todos, ademas del prefijo de comandos
+
+youtube_dl.utils.bug_reports_message = lambda: ''
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+ffmpeg_options = {
+    'options': '-vn'
+}
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = ""
+        
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop 
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+        filename = data['title'] if stream else ytdl.prepare_filename(data)
+        return filename
 
 @client.event
 async def on_ready(): # establece que hacer una vez el bot este encendido, en este caso imprime un mensaje en pantalla
@@ -31,7 +69,7 @@ async def broma(ctx):
     response= requests.get(url, headers=headers)
     texto = json.loads(response.text)['data']
     # await ctx.send(texto)
-     # formateamos el texto para que nos suelte un chiste
+    # formateamos el texto para que nos suelte un chiste
     
     url = "https://google-translator9.p.rapidapi.com/v2"
 
@@ -54,6 +92,35 @@ async def broma(ctx):
     texto = respuesta['data']['translations'][0]['translatedText'] 
     
     await ctx.send(texto)
+
+@client.command(pass_context = True)
+async def unete(ctx):
+    if(ctx.author.voice):
+        channel = ctx.message.author.voice.channel
+        voice = await channel.connect()
+    else:
+        await ctx.send("No estas en ningun canal de voz, por favor unete a uno")
+
+@client.command(pass_context = True)
+async def vete(ctx):
+    if(ctx.voice_client):
+        await ctx.guild.voice_client.disconnect()
+        await ctx.send("Me he desconectado")
+    else:
+        await ctx.send("No estoy en ningun canal")
+
+@client.command(pass_context = True)
+async def play(ctx,url):
+    if(ctx.voice_client):
+        server = ctx.message.guild
+        voice_channel = server.voice_client
+        async with ctx.typing():
+            filename = await YTDLSource.from_url(url, loop=client.loop)
+            voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename))
+        await ctx.send('**Now playing:** {}'.format(filename))
+    else:
+        await ctx.send("No estoy conectada a ningun canal, por favor invocame con el comando !unete")
+
 
 client.run(token)
 
