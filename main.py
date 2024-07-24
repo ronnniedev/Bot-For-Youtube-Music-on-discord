315b85fb50832a8156ccb66d94a805c3d15900ec
ronnniedev <veronicapersonal1995@gmail.com>
2024-07-25 00:40:50 +0200

    Version 0_22

--- main.py
+++ main.py
@@ -3,46 +3,25 @@ from discord.ext import commands
 import yt_dlp as youtube_dl
 import asyncio
 import os
-import time
 from modulos import bromas
 # importamos variables de apikeys
 from apikeys import *
 
-
 client = commands.Bot(command_prefix='!', intents=discord.Intents.all()) #Establece las propiedades del cliente, intents los permisos, lo inicializamos con todos, ademas del prefijo de comandos
-filename = ""
 
 @client.event
 async def on_ready(): # establece que hacer una vez el bot este encendido, en este caso imprime un mensaje en pantalla
     print("Zitra ha sido conectada con exito!")
     print("----------------------------------")
     channel = client.get_channel(1259826510727086252)
-    await channel.send("-------Version 0.2-------\n\nEste bot ahora esta en la nube!\n\n--!play hara que el bot se una a tu canal de voz y reproduzca un audio"+
-                       "\n\n--!stop hara que se pare y se vaya del canal")
-
-@client.command(pass_context = True) # si escribimos el comando, hola, escribira esto en pantalla
-async def test(ctx):
-    os.remove("hola.txt")
-
-@client.command(pass_context = True) # si escribimos el comando, hola, escribira esto en pantalla
-async def broma(ctx):
-    await bromas.bromaActua(ctx)
-
-@client.command(pass_context = True)
-async def unete(ctx):
-    await ctx.send("Este comando ya no funciona: Usa !play + url de tu video para que me conecte a tu canal :)")
-
-@client.command(pass_context = True)
-async def vete(ctx):
-    if(ctx.voice_client):
-        await ctx.guild.voice_client.disconnect()
-        await ctx.send("Me he desconectado")
-    else:
-        await ctx.send("No estoy en ningun canal")
+    await channel.send("-------Version 0.22-------\nActualizacion de emergencia realizada\n"
+                       +"--Comando -->!ayuda introducido explicando el funcionamiento de la nueva botonera\n"
+                       +"--Botonera mejorada, ahora el bot distingue si esta en funcionamiento en un canal\n"
+                       +"--Optimizacion del reproductor, ahora no se puede abusar del comando -!play")
 
 youtube_dl.utils.bug_reports_message = lambda: ''
 ytdl_format_options = {
-    'format': 'mp4',
+    'format': 'bestaudio/best',
     'restrictfilenames': True,
     'noplaylist': True,
     'nocheckcertificate': True,
@@ -58,7 +37,7 @@ ffmpeg_options = {
 }
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.1):
+    def __init__(self, source, *, data, volume=0.5):
         super().__init__(source, volume)
         self.data = data
         self.title = data.get('title')
@@ -73,54 +52,95 @@ class YTDLSource(discord.PCMVolumeTransformer):
             data = data['entries'][0]
         filename = data['title'] if stream else ytdl.prepare_filename(data)
         return filename
- 
+
+@client.command(pass_context = True) # si escribimos el comando, hola, escribira esto en pantalla
+async def broma(ctx):
+    await bromas.bromaActua(ctx)
+
+@client.command(pass_context = True)
+async def ayuda(ctx):
+    await ctx.send("--Comandos de tu servidora Zitra--\n\n"
+           + "--!broma :Te escribire una maginifica broma que hara que te deleites en ti mismo\n"
+           + "--!play + url de youtube : Si no estoy en un canal de voz me unire y reproducire el video que quieras, recuerda que los streams no los leeree\n"
+           + "--!para : parare el audio que este reproduciendo\n"
+           + "--!continua : reiniciare el audio que allas pausado\n"
+           + "--!stop : si quieres que me vaya del canal y parar la cancion actual escribe este comando\n\n"
+           + "(>owo)>Ante cualquier problema contacta con mi creadora veroshe_her <(owo<)") 
+
+@client.command(pass_context = True)
+async def unete(ctx):
+    await ctx.send("Comando desactivado, por favor usa !play + url de youtube de tu cancion para que el bot se una a tu canal :)")
+
 @client.command(pass_context = True)
 async def play(ctx,url):
+
     global filename
-    channel = ctx.message.author.voice.channel
-    voice = await channel.connect()
+
+    if not ctx.voice_client:
+       channel = ctx.message.author.voice.channel
+       voice = await channel.connect()
+       
     if(ctx.voice_client):
-        server = ctx.message.guild
-        voice_channel = server.voice_client
-        async with ctx.typing():
-            filename = await YTDLSource.from_url(url, loop=client.loop)
-            def after_playing_callback(error):
-                coro = after_playing(ctx, filename)
-                fut = asyncio.run_coroutine_threadsafe(coro, client.loop)
-                try:
-                    fut.result()
-                except Exception as e:
-                    print(f"Error al desconectar: {e}")
-            voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename),after=after_playing_callback)
-        await ctx.send("Now playing requested by @{} : {}".format(ctx.author.name,filename))
-    else:
-        await ctx.send("No estoy conectada a ningun canal, por favor invocame con el comando !unete")
+        voice = discord.utils.get(client.voice_clients,guild=ctx.guild)
+        if voice.is_playing():
+           await ctx.send("Estoy reproduciendo un audio, espera a que termine o usa el comando -->!stop para que acabe la cancion actual")
+        else:  
+            server = ctx.message.guild
+            voice_channel = server.voice_client
+            async with ctx.typing():
+                filename = await YTDLSource.from_url(url, loop=client.loop)
+                def after_playing_callback(error):
+                    coro = after_playing(ctx, filename)
+                    fut = asyncio.run_coroutine_threadsafe(coro, client.loop)
+                    try:
+                        fut.result()
+                    except Exception as e:
+                        print(f"Error al desconectar: {e}")
+                voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename),after=after_playing_callback)
+            await ctx.send('Reproduciendo: {}'.format(url))
+    else: 
+        await ctx.send('ERROR: No puedo unirme a tu canal')
 
 async def after_playing(ctx, filename):
     os.remove(filename)
 
 @client.command(pass_context = True)
 async def para(ctx):
-    voice = discord.utils.get(client.voice_clients,guild=ctx.guild)
-    if voice.is_playing():
-        voice.pause()   
+    if(ctx.voice_client):
+        voice = discord.utils.get(client.voice_clients,guild=ctx.guild)
+        if voice.is_playing():
+            voice.pause()   
+        elif voice.is_paused():
+            await ctx.send("El audio ya esta en pausa")
+        else:
+            await ctx.send("No estoy reproduciendo audio en este momento")
     else:
-        await ctx.send("No estoy reproduciendo audio en este momento")
+        await ctx.send("No estoy en ningun canal en este momento")
+
 
 @client.command(pass_context = True)
 async def continua(ctx):
-    voice = discord.utils.get(client.voice_clients,guild=ctx.guild)
-    if voice.is_paused():
-        voice.resume()   
+    if(ctx.voice_client):
+        voice = discord.utils.get(client.voice_clients,guild=ctx.guild)
+        if voice.is_paused():
+            voice.resume()   
+        else:
+            await ctx.send("La cancion ya esta sonando")
     else:
-        await ctx.send("No estoy reproduciendo audio en este momento")
+        await ctx.send("No estoy en ningun canal en este momento") 
 
 @client.command(pass_context = True)
 async def stop(ctx):
-    voice = discord.utils.get(client.voice_clients,guild=ctx.guild)
-    voice.stop()
-    await ctx.voice_client.disconnect() 
-    os.remove(filename)   
+    if(ctx.voice_client):
+        voice = discord.utils.get(client.voice_clients,guild=ctx.guild)
+        if voice.is_playing():
+            voice.stop() 
+            await ctx.voice_client.disconnect()  
+            os.remove(filename)
+        else: 
+            await ctx.voice_client.disconnect()    
+    else:
+        await ctx.send("No estoy en ningun canal en este momento")      
 
 client.run(token)
 
